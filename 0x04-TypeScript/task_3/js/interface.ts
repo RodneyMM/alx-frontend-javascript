8cb9c34a7ffe8c6ea64d026d781de5d1cd1ceb50
Rodney M Munyengeterwa <rodneym.munyengeterwa@gmail.com>
2025-06-04 00:12:38 +0200

    Task 3

---
+++ 0x04-TypeScript/task_3/js/interface.ts
@@ -0,0 +1,9 @@
+// Define type alias for RowID
+export type RowID = number;
+
+// Define interface for a row element
+export interface RowElement {
+  firstName: string;
+  lastName: string;
+  age?: number;
+}
